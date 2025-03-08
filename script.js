006e61644f9894d4ce01af7a0e8b96f315261283
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-03-07 22:49:36 -0300

    materias da faculdade

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ if (pt >= 5) {
   console.log("Aprovado. Sua nota é: ", pt);
 } else {
   console.log("Realizar prova subsitutiva. Sua nota é:", pt);
-  let ps = parseFloat(prompt("Digite a prova da nota substitutiva"));
+  let ps = parseFloat(prompt("Digite a prova da nota substitutiva:"));
   if (ps > p1 && ps < p2) {
-    p2 = ps;
-    pt = p1 * 0.4 + p2 * 0.6;
+    let p2 = ps;
+    let pt = p1 * 0.4 + p2 * 0.6;
   }
 
   if (pt >= 5) {
-    console.log("Aluno reprovado");
+    console.log("Aluno reprovado", ps);
   } else {
-    console.log("Aluno aprovado");
+    console.log("Aluno aprovado", ps);
   }
 }
