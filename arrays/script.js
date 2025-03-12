ab93ccaf4575f6bc25ac326cd71444516e8e47d8
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-03-12 09:21:14 -0300

    entendendo sobre Arrays

---
+++ arrays/script.js
@@ -0,0 +1,40 @@
+/*
+const a = [1, 2, 3, 4];
+const b = [2, 0, 3, 5];
+const c = a * b;
+console.log("Sua conta é", c);
+console.log("Vetor A", a);
+console.log("Vetor B", b);
+*/
+
+/*
+let d = Array.of(1, 2, 3);
+console.log(d)
+*/
+
+//matriz
+/*let a = [
+  [0, 1, 2],
+  [2, 3, 4],
+  [6, 0, 7],
+];
+
+console.log(a[1][2]);
+*/
+
+/*h = [1, 2, 3, 4, 5, 6];
+console.log(h, (length = 3));
+*/
+
+//usando a tag push para adicionar um elemento no final do array.
+/*let a = [1, 3, 5, 7, 10];
+a.push(2);
+    
+console.log("vetor:", a);
+*/
+
+let a = [1, 3, 5, 7, 10];
+a.unshift("a");
+a.unshift(0, -1);
+
+console.log("vetor:", a);
